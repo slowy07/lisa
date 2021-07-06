5f6ed038b7cf060b40ea8e75ec974c4772ea5d29
Matthew McGovern (HE/HIM) <mamcgove@microsoft.com>
2021-07-06 12:13:18 -0700

    add some work installing ubuntu dependencies

--- microsoft/testsuites/dpdk/dpdksuite.py
+++ microsoft/testsuites/dpdk/dpdksuite.py
@@ -8,6 +8,8 @@ from assertpy import assert_that
 from lisa import Node, TestCaseMetadata, TestSuite, TestSuiteMetadata
 from lisa.features import Sriov
 from lisa.testsuite import simple_requirement
+from lisa.operating_system import Ubuntu, Redhat, CentOs, Oracle
+from lisa.tools import Git
 
 
 @TestSuiteMetadata(
@@ -18,8 +20,47 @@ from lisa.testsuite import simple_requirement
     """,
 )
 class dpdk(TestSuite):
-    def _install_dpdk(self, node: Node, case_path: Path) -> None:
-        pass
+    _ubuntu_packages = [
+        "librdmacm-dev",
+        "librdmacm1",
+        "build-essential",
+        "libnuma-dev",
+        "libmnl-dev",
+        "libelf-dev",
+        "meson",
+        "rdma-core",
+        "librdmacm-dev",
+        "librdmacm1",
+        "build-essential",
+        "libnuma-dev",
+        "libmnl-dev",
+        "libelf-dev",
+        "dpkg-dev",
+        "pkg-config",
+        "python3-pip",
+        "python3-pyelftools",
+        "python-pyelftools",
+    ]
+
+    _dpdk_github = "https://github.com/DPDK/dpdk.git"
+
+    def _install_dpdk_dependencies(self, node: Node) -> None:
+        if isinstance(node.os, Ubuntu):
+            node.os.install_packages(self._ubuntu_packages)
+            self.log.info("Packages installed for Ubunutu")
+        elif isinstance(node.os, Redhat) or isinstance(node.os, CentOs):
+            node.os.install_packages(
+                ["groupinstall", "'Infiniband Support'"], signed=False
+            )  # todo gross hack to support groupinstall
+            result = node.execute(
+                "dracut --add-drivers 'mlx4_en mlx4_ib mlx5_ib' -f"
+            )  # add mellanox drivers
+            self.log.debug("\n".join([result.stdout, result.stderr]))
+
+    def _install_dpdk(self, node: Node) -> None:
+        git_tool = node.tools[Git]
+        git_tool.clone(self._dpdk_github, cwd=node.working_path)
+        self.log.info(node.execute("ls -la", cwd=node.working_path).stdout)
 
     @TestCaseMetadata(
         description="""
@@ -31,4 +72,11 @@ class dpdk(TestSuite):
         priority=1,
     )
     def check_dpdk_build(self, case_name: str, node: Node) -> None:
-        pass
+        sriov_feature = node.features[Sriov]
+        sriov_is_enabled = sriov_feature.enabled()
+        self.log.info(f"Verify SRIOV is enabled: {sriov_is_enabled}")
+        assert_that(sriov_is_enabled).described_as(
+            "SRIOV was not enabled for this test node."
+        )
+        self._install_dpdk_dependencies(node)
+        self._install_dpdk(node)
