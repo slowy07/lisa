1d2cbb6fdbd6b2b72d6e661e78ce056d1ceaa88d
Matthew G McGovern <mamcgove@microsoft.com>
2021-07-07 09:41:11 -0700

    some code cleanup

--- microsoft/testsuites/dpdk/dpdksuite.py
+++ microsoft/testsuites/dpdk/dpdksuite.py
@@ -20,6 +20,28 @@ from lisa.tools import Git
     """,
 )
 class dpdk(TestSuite):
+
+    @TestCaseMetadata(
+        description="""
+            This test case checks DPDK can be built and installed correctly.
+        """,
+        requirement=simple_requirement(
+            supported_features=[Sriov],
+        ),
+        priority=1,
+    )
+    def check_dpdk_build(self, case_name: str, node: Node) -> None:
+        sriov_feature = node.features[Sriov]
+        sriov_is_enabled = sriov_feature.enabled()
+        self.log.info(f"Verify SRIOV is enabled: {sriov_is_enabled}")
+        assert_that(sriov_is_enabled).described_as(
+            "SRIOV was not enabled for this test node."
+        )
+        self._install_dpdk_dependencies(node)
+        self._hugepages_init(node)
+        self._hugepages_enable(node)
+        self._install_dpdk(node)
+
     _ubuntu_packages = [
         "librdmacm-dev",
         "librdmacm1",
@@ -57,26 +79,26 @@ class dpdk(TestSuite):
             )  # add mellanox drivers
             self.log.debug("\n".join([result.stdout, result.stderr]))
 
+    def _execute_expect_zero(self, node: Node, cmd: str) -> None:
+        result = node.execute(cmd, sudo=True)
+        assert_that(result.exit_code).is_zero().described_as(
+            f"{cmd} failed with code {result.exit_code} and stdout+stderr:" +
+            f"\n{result.stdout}\n=============\n{result.stderr}\n=============\n"
+        )
+
+    def _hugepages_init(self, node: Node) -> None:
+        self._execute_expect_zero(node, "mkdir -p /mnt/huge")
+        self._execute_expect_zero(node, "mkdir -p /mnt/huge-1G")
+        self._execute_expect_zero(node, "mount -t hugetlbfs nodev /mnt/huge")
+        self._execute_expect_zero(node, "mount -t hugetlbfs nodev /mnt/huge-1G -o 'pagesize=1G'")
+
+    def _hugepages_enable(self, node: Node) -> None:
+        self._execute_expect_zero(node, "echo 4096 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages")
+        self._execute_expect_zero(node, "echo 1 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages")
+        result = node.execute("grep -i huge /proc/meminfo && ls /mnt/", shell=True)
+        self.log.info(f"hugepages status \n{result.stdout}")
+
     def _install_dpdk(self, node: Node) -> None:
         git_tool = node.tools[Git]
         git_tool.clone(self._dpdk_github, cwd=node.working_path)
         self.log.info(node.execute("ls -la", cwd=node.working_path).stdout)
-
-    @TestCaseMetadata(
-        description="""
-            This test case checks DPDK can be built and installed correctly.
-        """,
-        requirement=simple_requirement(
-            supported_features=[Sriov],
-        ),
-        priority=1,
-    )
-    def check_dpdk_build(self, case_name: str, node: Node) -> None:
-        sriov_feature = node.features[Sriov]
-        sriov_is_enabled = sriov_feature.enabled()
-        self.log.info(f"Verify SRIOV is enabled: {sriov_is_enabled}")
-        assert_that(sriov_is_enabled).described_as(
-            "SRIOV was not enabled for this test node."
-        )
-        self._install_dpdk_dependencies(node)
-        self._install_dpdk(node)
